d773acd1b490a40d55ce129aae3737be562d0daa
Aleksandar Kostovski <aleksandar.kostovski@ttc.media>
2025-11-22 23:06:50 +0100

    seed.ts fix

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@/lib/generated/prisma";
-import sampleData from './sample-data'
+import { PrismaClient } from "@prisma/client";
+import sampleData from '../db/sample-data'
 
 async function main() {
     const prisma = new PrismaClient();
